038a4319d9fe952ac7bac2fa6682e7dc9a6b8f56
Markus Blaschke <mblaschke82@gmail.com>
2016-03-09 22:33:32 +0100

    Disable DOCKER_TLS_VERIFY for docker-compose > 1.6.0

--- provision/docker-init.sh
+++ provision/docker-init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export DOCKER_HOST=tcp://192.168.56.2:2375
-export DOCKER_TLS_VERIFY=
+#export DOCKER_TLS_VERIFY=
 
 echo "
 # Docker settings
 export DOCKER_HOST=tcp://192.168.56.2:2375
-export DOCKER_TLS_VERIFY=
+#export DOCKER_TLS_VERIFY=
 " >> ~/.bashrc >> ~/.zshrc
